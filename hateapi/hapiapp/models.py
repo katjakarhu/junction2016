f847845ede72bdf3796171d84f4102e9574c3292
Katja Karhu <katja.karhu@iki.fi>
2016-11-25 22:27:53 +0200

    Very basic furhatmodel

--- hateapi/hapiapp/models.py
+++ hateapi/hapiapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class FakeNews(models.Model):
+    url = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    source = models.CharField(max_length=255)
+    isFake = models.BooleanField()
--- hateapi/hateapi/urls.py
+++ hateapi/hateapi/urls.py
@@ -13,12 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
-from django.conf.urls import include
 
+from django.conf.urls import url, include
+from hapiapp.models import FakeNews
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class FakeNewsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = FakeNews
+        fields = ('url', 'name', 'source', 'isfake')
+
+# ViewSets define the view behavior.
+class FakeNewsViewSet(viewsets.ModelViewSet):
+    queryset = FakeNews.objects.all()
+    serializer_class = FakeNewsSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'fakenews', FakeNewsViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
