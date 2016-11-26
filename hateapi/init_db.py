e15d880dd9a49057cfe353c9e758d01c0ad8e487
= <=>
2016-11-26 08:44:03 +0200

    init_db script

---
+++ hateapi/init_db.py
@@ -0,0 +1,94 @@
+from hapiapp.models import FakeSite
+
+site = FakeSite(site='AmericanNews.com')
+site.save()
+
+site = FakeSite(site='BigAmericanNews.com')
+site.save()
+
+site = FakeSite(site='twitter.com/capnews')
+site.save()
+
+site = FakeSite(site='ChristWire.org')
+site.save()
+
+site = FakeSite(site='CivicTribune.com')
+site.save()
+
+site = FakeSite(site='ClickHole.com')
+site.save()
+
+site = FakeSite(site='CreamBMP.com')
+site.save()
+
+site = FakeSite(site='DCGazette.com')
+site.save()
+
+site = FakeSite(site='DailyCurrant.com')
+site.save()
+
+site = FakeSite(site='DCClothesLine.com')
+site.save()
+
+site = FakeSite(site='DerfMagazine.com')
+site.save()
+
+site = FakeSite(site='DrudgeReport.com.co')
+site.save()
+
+site = FakeSite(site='DuhProgressive.com')
+site.save()
+
+site = FakeSite(site='EmpireNews.com')
+site.save()
+
+site = FakeSite(site='EnduringVision.com')
+site.save()
+
+site = FakeSite(site='www.cc.com/indecision')
+site.save()
+
+site = FakeSite(site='MSNBC.co')
+site.save()
+
+site = FakeSite(site='MSNBC.website')
+site.save()
+
+site = FakeSite(site='MediaMass.net')
+site.save()
+
+site = FakeSite(site='NationalReport.net')
+site.save()
+
+site = FakeSite(site='NewsBiscuit.com')
+site.save()
+
+site = FakeSite(site='News-Hound.com')
+site.save()
+
+site = FakeSite(site='NewsMutiny.com')
+site.save()
+
+site = FakeSite(site='PoliticalEars.com')
+site.save()
+
+site = FakeSite(site='Private-eye.co.uk')
+site.save()
+
+site = FakeSite(site='RealNewsRightNow.com')
+site.save()
+
+site = FakeSite(site='RileNews.com')
+site.save()
+
+site = FakeSite(site='Sprotspickle.com')
+site.save()
+
+site = FakeSite(site='TheNewsNerd.com')
+site.save()
+
+site = FakeSite(site='TheUsPatriot.com')
+site.save()
+
+site = FakeSite(site='WitScience.org')
+site.save()
